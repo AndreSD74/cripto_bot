012145879d780802eb391a6c53fa8abb59e8ca7c
andresd74 <andre.silveira.david@gmail.com>
2021-11-25 15:01:06 -0300

    printing candle

--- test_binance.py
+++ test_binance.py
@@ -1,11 +1,16 @@
-# pip install websocket-client
+# Libs
+# # pip install websocket-client
 
+# Docs
 # https://github.com/binance/binance-spot-api-docs/blob/master/rest-api.md
+
+# Videos
+# https://www.youtube.com/watch?v=z2ePTq-KTzQ
 
 # wss://stream.binance.com:9443
 
-import websocket, json
 
+import websocket, json
 # socket_tst = 'wss://stream.binance.com:9443/ws/btcusdt@kline_1m'
 
 # Bitcoin US$
@@ -18,7 +23,18 @@ socket_tst = f'wss://stream.binance.com:9443/ws/{coin_param}t@kline_{interval}'
 
 # function is called when receive a message:
 def on_message(ws, message):
-    print(message)
+    # print(message)
+    json_message = json.loads(message)
+    candle = json_message['k']
+    is_candle_closed = candle['x']
+    close = candle['c']
+    high = candle['h']
+    low = candle['l']
+    vol = candle['v']
+
+    if is_candle_closed:
+        print('close: {} high: {} low: {}'.format(close, high, low))
+
 
 # function is called when websocket is closed:
 def on_close(ws, message):
