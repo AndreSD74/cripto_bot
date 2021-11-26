902119439532dbebecbd6c79b588edaa395b8022
andresd74 <andre.silveira.david@gmail.com>
2021-11-26 18:52:14 -0300

    using Binance API

---
+++ test_binance_vision.py
@@ -0,0 +1,28 @@
+# https://testnet.binance.vision/
+# https://algotrading101.com/learn/binance-python-api-guide/
+
+import os
+binance_api = os.environ.get('binance_api')
+binance_secret = os.environ.get('binance_secret')
+
+from binance.client import Client
+
+client = Client(binance_api, binance_secret)
+
+# TEST's URL
+client.API_URL = 'https://testnet.binance.vision/api'
+
+# get account information
+print(client.get_account())
+
+# get balance for a specific asset only (BTC)
+print(client.get_asset_balance(asset='BTC'))
+
+# get latest price from Binance API
+btc_price = client.get_symbol_ticker(symbol="BTCUSDT")
+# print full output (dictionary)
+print(btc_price)
+
+#  print(client.get_exchange_info())
+
+
